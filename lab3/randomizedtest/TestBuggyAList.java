052c461230af2665683ae8e637c65033dc555488
Tau <3067434565@qq.com>
2025-07-27 21:57:07 +0800

    lab3 done

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -9,4 +9,53 @@ import static org.junit.Assert.*;
  */
 public class TestBuggyAList {
   // YOUR TESTS HERE
+    @Test
+    public void testThreeAddThreeRemove() {
+        AListNoResizing<Integer> aListNoResizing = new AListNoResizing<>();
+        BuggyAList<Integer> buggyAList = new BuggyAList<>();
+
+        aListNoResizing.addLast(4);
+        aListNoResizing.addLast(5);
+        aListNoResizing.addLast(6);
+
+        buggyAList.addLast(4);
+        buggyAList.addLast(5);
+        buggyAList.addLast(6);
+
+        assertEquals(aListNoResizing.removeLast(), buggyAList.removeLast());
+        assertEquals(aListNoResizing.removeLast(), buggyAList.removeLast());
+        assertEquals(aListNoResizing.removeLast(), buggyAList.removeLast());
+    }
+
+    @Test
+    public void randomizedTest() {
+        AListNoResizing<Integer> L = new AListNoResizing<>();
+        BuggyAList<Integer> BL = new BuggyAList<>();
+
+        int N = 500000;
+        for (int i = 0; i < N; i += 1) {
+            int operationNumber = StdRandom.uniform(0, 4);
+            if (operationNumber == 0) {
+                // addLast
+                int randVal = StdRandom.uniform(0, 100);
+                L.addLast(randVal);
+                BL.addLast(randVal);
+            } else if (operationNumber == 1) {
+                // size
+                int size = L.size();
+                int sizeBL = BL.size();
+                assertEquals(size, sizeBL);
+            } else if (operationNumber == 2 && L.size() > 0) {
+                //getLast
+                int last = L.getLast();
+                int lastBL = BL.getLast();
+                assertEquals(last, lastBL);
+            } else if (operationNumber == 3 && L.size() > 0) {
+                //removeLast
+                int last = L.removeLast();
+                int lastBL = BL.removeLast();
+                assertEquals(last, lastBL);
+            }
+        }
+    }
 }
