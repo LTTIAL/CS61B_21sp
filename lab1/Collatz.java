def354df8a881b4517f1103c7ac0c44b55a4173a
Tau <3067434565@qq.com>
2025-07-31 15:50:29 +0800

    Collatz.java back to the version unfinished

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -3,13 +3,14 @@
  */
 public class Collatz {
 
-    /** input: int
-     *  output: Collatz number */
+    /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n % 2 == 1) {
+        if (n  == 128) {
+            return 1;
+        } else if (n == 5) {
             return 3 * n + 1;
         } else {
-            return n / 2;
+            return n * 2;
         }
     }
 
